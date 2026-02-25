93eba8b5794818f12f2072e6d7ec0f3cc49b980c
LucasDev <lucasmndes.dev@gmail.com>
2026-02-25 14:01:21 -0300

    implement GET appointments and details, and POST book appointment and cancel

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,21 @@ import { AuthModule } from '@thallesp/nestjs-better-auth';
 import { auth } from './auth/auth.config';
 import { AdminModule } from './admin/admin.module';
 import { AppointmentsModule } from './appointments/appointments.module';
+import { ResendModule } from 'nest-resend';
+
+if (!process.env.RESEND_API_KEY) {
+  throw new Error('RESEND_API_KEY is not defined in environment variables');
+}
 
 @Module({
-  imports: [AuthModule.forRoot({ auth }), AdminModule, AppointmentsModule],
+  imports: [
+    AuthModule.forRoot({ auth }),
+    AdminModule,
+    AppointmentsModule,
+    ResendModule.forRoot({
+      apiKey: process.env.RESEND_API_KEY,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
